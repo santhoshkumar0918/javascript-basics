fddfb5b3afae9c3902f5298747f05b5dac7f8df4
santhoshkumar0918 <158072171+santhoshkumar0918@users.noreply.github.com>
2024-03-02 14:16:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+    let attempts = 0;
+    let guess ;
+    let running = true;
 
+    while(running){
+        guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`);
+        guess = Number(guess);
+
+        if(isNaN(guess)){
+            window.alert("please enter a valid number");
+        }
+        else if(guess < minNum ||  guess > maxNum){
+       window.alert("please enter a valid number")
+        }
+        else{
+            attempts++;
+            if(guess < answer){
+                window.alert("its too low ! try again");
+            }
+            else if(guess > answer){
+            window.alert("its too high try again");
+        }
+        else{
+            window.alert(`CORRECT! the answer was ${answer}. it took ${attempts} to complete`);
+            running = false;
+        }
+    }
+    }
